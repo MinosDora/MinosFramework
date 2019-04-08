8618eb0fcc23f1db7637cc2ac11949a5169f9ba0
MinotaurosD <doraemon1970@163.com>
2019-04-08 20:43:12 +0800

    test gcalloc

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using MinoFramework;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    private void OnGUI()
+    {
+        if (GUI.Button(new Rect(100, 100, 100, 100), "Add"))
+        {
+            EventMgr.Instance.AddListener(MinoFramework.EventType.MyType, (Action)MyFunc);
+        }
+        else if (GUI.Button(new Rect(100, 200, 100, 100), "Delete"))
+        {
+            EventMgr.Instance.RemoveListener(MinoFramework.EventType.MyType, (Action)MyFunc);
+        }
+        else if (GUI.Button(new Rect(100, 300, 100, 100), "Trigger"))
+        {
+            EventMgr.Instance.TriggerEvent(MinoFramework.EventType.MyType);
+        }
+    }
+
+    public void MyFunc()
+    {
+        Debug.LogError("哈哈哈哈哈哈哈哈哈哈哈");
+    }
+}
