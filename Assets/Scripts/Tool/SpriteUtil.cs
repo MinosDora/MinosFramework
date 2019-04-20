cb954286553da20e94132098528d80c2d56bb4a4
MinotaurosD <doraemon1970@163.com>
2019-04-20 13:47:03 +0800

    add sprite util

---
+++ Assets/Scripts/Tool/SpriteUtil.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+/// <summary>
+/// Sprite工具类
+/// </summary>
+public static class SpriteUtil
+{
+    /// <summary>
+    /// 根据Texture2D创建Sprite
+    /// </summary>
+    /// <param name="texture2D"></param>
+    /// <returns></returns>
+    public static Sprite CreateSprite(Texture2D texture2D)
+    {
+        return Sprite.Create(texture2D, new Rect(0f, 0f, texture2D.width, texture2D.height), Vector2.one * 0.5f, 100, 0, SpriteMeshType.FullRect);
+    }
+}
